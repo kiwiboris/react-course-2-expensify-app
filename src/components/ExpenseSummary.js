e14b522e1789ad5dc905bac951bed7f7931aefb2
Boris Velikovich <kiwiboris@gmail.com>
2020-01-01 20:02:20 -0500

    Changed add expense summary

--- src/components/ExpenseSummary.js
+++ src/components/ExpenseSummary.js
@@ -4,16 +4,17 @@ import numeral from 'numeral';
 import selectExpenses from '../selectors/expenses';
 import selectTotalExpenses from '../selectors/expenses-total';
 
-export const ExpenseSummary = (props) => (
-    <div>
-        { props.expenseCount === 0 ? (
-            <p></p>
-        ) : (
-            <p>Viewing {props.expenseCount} expenses totalling {numeral(props.expensesTotal / 100).format('$0,0.00')}</p>
-        )
-    }
-    </div>
-);
+export const ExpenseSummary = ({expenseCount, expensesTotal }) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal / 100 ).format('$0,0.00');
+    return (
+        <div>
+        <h1>
+        Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}
+        </h1>
+        </div>  
+        );
+    };
 
 const mapStateToProps = (state) => {
     const visibleExpenses = selectExpenses(state.expenses, state.filters);
--- src/tests/components/ExpenseSummary.test.js
+++ src/tests/components/ExpenseSummary.test.js
@@ -3,12 +3,12 @@ import { shallow } from 'enzyme';
 import { ExpenseSummary } from '../../components/ExpenseSummary';
 import expenses from '../fixtures/expenses';
 
-test('should render ExpenseSummary with empty message', () => {
-    const wrapper = shallow(<ExpenseSummary />);
+test('should render ExpenseSummary with 1 expense', () => {
+    const wrapper = shallow(<ExpenseSummary expenseCount={1} expensesTotal={235} />);
     expect(wrapper).toMatchSnapshot();
 });
 
-test('should render ExpenseSummary with all expenses', () => {
-    const wrapper = shallow(<ExpenseSummary expenseCount={2} expensesTotal={200} />);
+test('should render ExpenseSummary with multiple expenses', () => {
+    const wrapper = shallow(<ExpenseSummary expenseCount={23} expensesTotal={2344500} />);
     expect(wrapper).toMatchSnapshot();
 });
